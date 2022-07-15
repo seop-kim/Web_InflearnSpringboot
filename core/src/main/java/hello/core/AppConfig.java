f995e631e26b8ae8657a5192fc8a9a8af04f7e63
tseop <77163842+tseop@users.noreply.github.com>
2022-07-15 11:50:12 +0900

    김영한의 스프링 부트 학습 업로드
    스프링 핵심 원리 - 기본편
    - 스프링 컨테이너와 스프링 빈
     - 스프링 빈 조회 - 동일한 타입이 둘 이상
    까지

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,37 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    // 회원 repo
+    @Bean
+    public MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // 할인 정책 repo
+    @Bean
+    public DiscountPolicy getDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
