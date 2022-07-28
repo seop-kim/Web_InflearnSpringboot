46af3bbd2461a11d3e2a5b7e211a67f17c5b9f0a
tseop <77163842+tseop@users.noreply.github.com>
2022-07-28 17:48:50 +0900

    실전! 스프링 부트와 JPA 활용1 업로드
    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발]
    강의 시작
    
    - 회원 도메인 개발까지 진행

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager entityManager;
+
+    @Test
+    @DisplayName("회원가입")
+    public void save() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        entityManager.flush();
+        assertEquals(member, memberRepository.findOne(savedId));
+
+    }
+
+    @Test
+    @DisplayName("중복 회원 예외")
+    public void validateDuplicateMemberException() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+}
