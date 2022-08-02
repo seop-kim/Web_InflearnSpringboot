d233f9339201bab466f3dd2944a76d047af572cc
tseop <77163842+tseop@users.noreply.github.com>
2022-08-02 13:15:30 +0900

    실전! 스프링 부트와 JPA 활용1 업데이트
    실전! 스프링 부트와 JPA 활용1 업데이트
    - 주문 도메인 개발 까지

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,16 +16,35 @@ import java.util.List;
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "item_id")
-    private Long id;
+    @Id @GeneratedValue @Column(name = "item_id") private Long id;
 
     private String name;
     private int price;
     private int stockQuantity;
 
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
+    @ManyToMany(mappedBy = "items") private List<Category> categories = new ArrayList<>();
 
+    // == 비즈니스 로직 == //
+
+    /**
+     * 재고 수량 증가
+     *
+     * @param stockQuantity
+     */
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    /**
+     * 재고 수량 감소
+     *
+     * @param stockQuantity
+     */
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity -= stockQuantity;
+    }
 }
--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -2,19 +2,18 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
