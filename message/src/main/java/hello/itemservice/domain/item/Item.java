2dc3874b33f339f30fb9920981ae73cc6b49867f
tseop <77163842+tseop@users.noreply.github.com>
2022-08-24 16:27:16 +0900

    thymeleaf - spring form close & msg project open
    thymeleaf - 스프링 통합과 폼 완료,
    메시지, 국제화 학습 시작

--- message/src/main/java/hello/itemservice/domain/item/Item.java
+++ message/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,8 +2,6 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class Item {
 
@@ -11,11 +9,6 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
-
-    private Boolean open; // 판매여부
-    private List<String> regions; // 등록지역
-    private ItemType itemType; // 상품종류
-    private String deliveryCode; // 배송방식
 
     public Item() {
     }
