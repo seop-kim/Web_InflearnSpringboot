1faee1f0631e59bdb9b1f6cd6430cd39f24d8dcb
tseop <77163842+tseop@users.noreply.github.com>
2022-09-16 16:48:51 +0900

    실전 예제 복습

---
+++ jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id @GeneratedValue private Long id;
+    private String username;
+}
